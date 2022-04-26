9b610d1c796c2044e1d8d2408658ba3e6f0f5679
syuan7492 <kurosaki7492@gmail.com>
2022-04-26 23:06:28 +0800

    Add project files.

---
+++ EazyMath/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EazyMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Q q = new Q(Q.R.Next(2) == 1);
+            
+            while (true)
+            {
+                Console.Write($"{q.N1} {q.Op} {q.N2} = ");
+                
+                if (!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine("\n感謝來玩~有緣再會~");
+                    return;
+                }
+
+                if (q.Check(i))
+                {
+                    Console.WriteLine("答對囉!!\n");
+                    q = new Q(Q.R.Next(2) == 1);
+                }
+
+                else Console.WriteLine("答錯囉!!再試試看~\n");
+            }
+        }
+    }
+}
---
+++ EazyMath/Q.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EazyMath
+{
+    class Q
+    {
+        //建構式
+        public Q(bool add)
+        {
+            if (add) Op = "+";
+            else Op = "*";
+            N1 = R.Next(1, 10);
+            N2 = R.Next(1, 10);
+        }
+
+        //屬性
+        public static Random R = new Random();
+        public int N1 { get; private set; }
+        public String Op { get; private set; }
+        public int N2 { get; private set; }
+
+        //方法
+        public bool Check(int a)
+        {
+            if (Op == "+") return (a == N1 + N2);
+            else return (a == N1 * N2);
+        }
+    }
+}
